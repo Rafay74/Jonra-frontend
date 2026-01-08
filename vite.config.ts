decf9370561b4852b0a406b078def38ca72d2fc2
Abdul Rafay <rafayh74@gmail.com>
2026-01-08 15:35:09 +0500

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,12 @@ import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-
-
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
